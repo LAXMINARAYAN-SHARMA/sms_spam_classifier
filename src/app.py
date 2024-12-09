ec51bad8528c0c21c938487768f36000d19c12eb
LAXMINARAYAN-SHARMA <researchanalystforapurpose@gmail.com>
2024-12-09 16:27:27 +0530

    final  here2

--- src/app.py
+++ src/app.py
@@ -2,9 +2,9 @@ import pickle
 import streamlit as st
 import utility_funct
 
-model=pickle.load(open(r'C:\Users\LENOVO\OneDrive\Desktop\MY_PROJECT_ML\src\pickle_files\model3.pkl','rb'))
+model=pickle.load(open(r'src/pickle_files/model3.pkl','rb'))
 # preproc=pickle.load(open(r'C:\Users\LENOVO\OneDrive\Desktop\MY_PROJECT_ML\src\pickle_files\preprocess_func.pkl','rb'))
-vectoriser=pickle.load(open(r'C:\Users\LENOVO\OneDrive\Desktop\MY_PROJECT_ML\src\pickle_files\vectoriser.pkl','rb'))
+vectoriser=pickle.load(open(r'src/pickle_files/vectoriser.pkl','rb'))
 
 st.title('EMAIL SPAM CLASSIFIER')
 input_sms=st.text_input('input your message here')
